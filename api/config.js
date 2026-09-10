f9e410907a2d54fd7d351ee2a914a225501a51e0
RuMaroNS <sdulimos@gmail.com>
2026-09-10 23:38:39 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,12 +1,6 @@
 export default function handler(req, res) {
-    // Разрешаем браузерам читать скрипт
-    res.setHeader('Content-Type', 'application/javascript');
-    
-    // Записываем ключи прямо в window до того, как сработает код страниц
-    const jsCode = `
-        window.SUPABASE_URL = "${process.env.SUPABASE_URL || ''}";
-        window.SUPABASE_ANON_KEY = "${process.env.SUPABASE_ANON_KEY || ''}";
-    `;
-    
-    return res.status(200).send(jsCode);
+  res.status(200).json({
+    SUPABASE_URL: process.env.SUPABASE_URL || '',
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || ''
+  });
 }
